399dff6da35f96f937126026d4d3112521f28053
Rahul Nayak <rahulnayak@electroons.com>
2019-07-17 10:50:17 +0530

    added NewTripSheetModal

--- src/Components/NewTripSheetModal.js
+++ src/Components/NewTripSheetModal.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
-const NewTripSheetModal = () => (
-    <Modal
-    isOpen={false}
-    contentLabel="Modal for new trip sheet name"
-    >
-        <p>Add new trip sheet name here</p>
-    </Modal>
-)
+class NewTripSheetModal extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  handleAddNewTrip() {}
+  render() {
+    return (
+      <Modal
+        className="new-trip-modal"
+        isOpen={false}
+        contentLabel="Modal for new trip sheet name"
+      >
+        <label htmlFor="new-trip">Name your trip:</label>
+        <input type="text" id="new-trip" />
+        <button>Add trip sheet</button>
+      </Modal>
+    );
+  }
+}
 
 export default NewTripSheetModal;
+
+// const state = {
+//   expenseSheets: ["expenseSheet1", "expenseSheet2"]
+// };
