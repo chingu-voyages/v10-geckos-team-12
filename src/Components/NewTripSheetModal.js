55c36e5d78021bbca0b637243f349b3abb86bec6
Rahul Nayak <rahulnayak@electroons.com>
2019-07-21 13:51:00 +0530

    added redux store

--- src/Components/NewTripSheetModal.js
+++ src/Components/NewTripSheetModal.js
@@ -1,27 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
 
-// class NewTripSheetModal extends React.Component {
-//   // constructor(props) {
-//   //   super(props);
-//   // }
-//   handleAddNewTrip() {}
-//   render() {
-//     return (
-//       <Modal
-//       className="new-trip-modal"
-//         isOpen={false}
-//         contentLabel="Modal for new trip sheet name"
-//       >
-//         <label htmlFor="new-trip">Name your trip:</label>
-//         <input type="text" id="new-trip" />
-//         <button>Add trip sheet</button>
-//       </Modal>
-      
-//     );
-//   }
-// }
-
 function NewTripSheetModal(props) {
   return (
     <Modal
@@ -30,6 +9,7 @@ function NewTripSheetModal(props) {
         contentLabel="Modal for new trip sheet name"
       >
         <label htmlFor="new-trip">Name your trip:</label>
+        <p className="add-new-person">Add Person</p>
         <input type="text" id="new-trip" />
         <button onClick={props.closeModal}>Add trip sheet</button>
       </Modal>
@@ -38,7 +18,3 @@ function NewTripSheetModal(props) {
 }
 
 export default NewTripSheetModal;
-
-// const state = {
-//   expenseSheets: ["expenseSheet1", "expenseSheet2"]
-// };
---
+++ src/Store/ConfigureStore.js
@@ -0,0 +1,45 @@
+import { createStore } from 'redux';
+
+// ADD_NEW_TRIP
+// ADD_NEW_EXPENSE
+// DELETE EXPENSE
+
+const store = createStore()
+
+const demoState = {
+    trips:[{
+        tripName: "Trip to New York",
+        travellers:["Will", "Mike", "Lucas", "Dustin"],
+        expenses:[{
+            expnese:"Flights",
+            whoPaid: "Mike",
+            forWhom:["Will", "Mike", "Lucas", "Dustin"],
+            howMuch:150
+        },
+        {
+            expnese:"Hotels",
+            whoPaid: "Lucas",
+            forWhom:["Will", "Mike", "Lucas", "Dustin"],
+            howMuch: 120
+        }   
+        ]
+    },
+    {
+        tripName: "Trip to StarCourt mall",
+        travellers:["Will", "Mike", "Lucas", "Dustin", "Eleven", "Max"],
+        expenses:[{
+            expnese:"Movies",
+            whoPaid: "Max",
+            forWhom:["Will", "Mike", "Lucas", "Dustin", "Eleven", "Max"],
+            howMuch: 30
+        },
+        {
+            expnese:"Popcorn",
+            whoPaid: "Dustin",
+            forWhom:["Lucas", "Dustin", "Eleven", "Max"],
+            howMuch: 5
+        }
+        ]
+    }
+]
+}
