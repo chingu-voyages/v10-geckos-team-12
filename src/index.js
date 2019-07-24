590a75f4c86b829517cf6d67f01b2be82a7cc188
Rahul Nayak <rahulnayak@electroons.com>
2019-07-24 11:31:11 +0530

    added HOC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRouter from './routers/AppRouter'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppRouter from "./routers/AppRouter";
+import addNewTrip from "./actions/addNewTrip";
+import deleteTrip from "./actions/deleteTrip";
+import store from './Store/ConfigureStore'
+import * as serviceWorker from "./serviceWorker";
+import uuid from "uuid";
 
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const LondonTrip = store.dispatch(
+  addNewTrip({
+    tripName: "London",
+    travellers: [{ id: uuid(), name: "Mike" }, { id: uuid(), name: "Dustin" }]
+  })
+);
+
+const ZurichTrip = store.dispatch(
+  addNewTrip({
+    tripName: "Zurich",
+    travellers: [{ id: uuid(), name: "Mike" }, { id: uuid(), name: "Eleven" }]
+  })
+);
+
+store.dispatch(deleteTrip({ tripId: ZurichTrip.trip.tripId }));
+
+store.dispatch(deleteTrip({ tripId: LondonTrip.trip.tripId }));
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,34 +4,6 @@ import Header from "../Components/Header";
 import SplitItAppDashBoardPage from "../Components/SplitItAppDashBoardPage";
 import PageNotFound from "../Components/PageNotFound";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { createStore } from "redux";
-import AppReducer from "../reducer/AppReducer";
-import addNewTrip from "../actions/addNewTrip";
-import deleteTrip from '../actions/deleteTrip';
-import store from '../Store/ConfigureStore';
-import uuid from "uuid";
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-const LondonTrip = store.dispatch(
-  addNewTrip({
-    tripName: "London",
-    travellers: [{ id: uuid(), name: "Mike" }, { id: uuid(), name: "Dustin" }]
-  })
-);
-
-const ZurichTrip = store.dispatch(
-  addNewTrip({
-    tripName: "Zurich",
-    travellers: [{ id: uuid(), name: "Mike" }, { id: uuid(), name: "Eleven" }]
-  })
-);
-
-store.dispatch(deleteTrip({ tripId: ZurichTrip.trip.tripId }));
-
-store.dispatch(deleteTrip({ tripId: LondonTrip.trip.tripId }));
 
 const AppRouter = () => (
   <BrowserRouter>
